571913cf29d00c06c283692c8cec7915c56427ef
Nicholas Goh <nicholasgoh09@gmail.com>
2026-08-18 23:32:41 +0800

    Changed the name of the chatbot to Alice and change the text accordingly. Used AI to make the changes for ASCII banner and name changes in README.md

---
+++ src/main/java/Alice.java
@@ -0,0 +1,16 @@
+public class Alice {
+    public static void main(String[] args) {
+        String banner = "_______________________________________\n"
+                + "    _    _     ___ ____ _____ \n"
+                + "   / \\  | |   |_ _/ ___| ____|\n"
+                + "  / _ \\ | |    | | |   |  _|  \n"
+                + " / ___ \\| |___ | | |___| |___ \n"
+                + "/_/   \\_\\_____|___\\____|_____|\n"
+                + "Hello! I'm Alice.\n"
+                + "What can I do for you?\n"
+                + "_______________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_______________________________________\n";
+        System.out.println(banner);
+    }
+}
