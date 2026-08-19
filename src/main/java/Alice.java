8e372407c28e8b6125de6ab6d2eff5ca76bb66b7
Nicholas Goh <nicholasgoh09@gmail.com>
2026-08-19 12:46:55 +0800

    Updated the code to echo commands and quit when bye is entered. Change the seperating underscores into a string object

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,6 +1,10 @@
+import java.util.Scanner; //in order to get inputs from user
+
 public class Alice {
     public static void main(String[] args) {
-        String banner = "_______________________________________\n"
+        String separator = "_______________________________________\n";
+        //Used Codex to redesign banner
+        String banner = separator
                 + "    _    _     ___ ____ _____ \n"
                 + "   / \\  | |   |_ _/ ___| ____|\n"
                 + "  / _ \\ | |    | | |   |  _|  \n"
@@ -8,9 +12,26 @@ public class Alice {
                 + "/_/   \\_\\_____|___\\____|_____|\n"
                 + "Hello! I'm Alice.\n"
                 + "What can I do for you?\n"
-                + "_______________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "_______________________________________\n";
+                + separator;
+ //               + "Bye. Hope to see you again soon!\n"
+ //               + separator;
         System.out.println(banner);
+
+        //Used Codex to find how to take in inputs from user
+        Scanner scanner = new Scanner(System.in); //object to read
+        while (true) {
+            String userInput = scanner.nextLine(); //take in input
+            System.out.println(separator);
+
+            if (userInput.equals("bye")) { //check if need to exit
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+            else {
+                System.out.println(userInput);
+                System.out.println(separator);
+            }
+        }
     }
 }
