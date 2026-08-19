c9974cfbc15a8506fac5d83ca2e6ff8e6d22e3ba
Nicholas Goh <nicholasgoh09@gmail.com>
2026-08-19 17:17:11 +0800

    Updated the code to mark and unmark tasks. Also created the Task.java file

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -19,7 +19,7 @@ public class Alice {
 
         //Used Codex to find how to take in inputs from user
         Scanner scanner = new Scanner(System.in); //object to read
-        String[] userInputs = new String[100];
+        Task[] tasks = new Task[100];
         int inputCount = 0;
         while (true) {
             String userInput = scanner.nextLine(); //take in input
@@ -29,14 +29,33 @@ public class Alice {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(separator);
                 break;
+
             } else if (userInput.equals("list")) { //print the list
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < inputCount; i++) {
-                    System.out.println((i + 1) + ". " + userInputs[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 System.out.println(separator);
+
+            } else if (userInput.startsWith("mark ")) { //used Codex to find out how to if statements for mark and unmark
+                int taskNo = Integer.parseInt(userInput.substring(5)); //5th char in string
+                Task task = tasks[taskNo - 1]; //5th task means 4 in array
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+                System.out.println(separator);
+
+            } else if (userInput.startsWith("unmark ")) {
+                int taskNo = Integer.parseInt(userInput.substring(7)); //7th char in string
+                Task task = tasks[taskNo - 1]; //5th task means 4 in array
+                task.unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
+                System.out.println(separator);
+
             } else { //add the string to list and print it
                 //store the string
-                userInputs[inputCount] = userInput;
+                tasks[inputCount] = new Task(userInput);
                 inputCount++;
 
                 System.out.println("added: " + userInput);
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    public String description;
+    public boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
